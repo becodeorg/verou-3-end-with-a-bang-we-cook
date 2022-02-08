f961076434b92759bda201dbab6c74a6e5700760
unknown <cskeith01@gmail.com>
2022-02-08 11:01:30 +0100

    WIP groceryList

--- groceryList/groceryList.js
+++ groceryList/groceryList.js
@@ -45,28 +45,19 @@ const createH1Element = () => {
 const groceryList = () => {
     // Link to HTML
     const main = document.querySelector("main");
-    // create
+
+    // create groceryListBody
     const groceryListBody = createDivElement();
     groceryListBody.className = ("groceryListBody");
     main.appendChild(groceryListBody);
 
+    // create groceryListTitle
     const groceryListTitle = createH1Element();
     groceryListTitle.innerHTML = ("Grocery List");
     groceryListBody.appendChild(groceryListTitle);
 
-    const infoCard = createDivElement();
-
-    section.appendChild(infoCard);
-    const locationH1 = createH1Element();
-    locationH1.innerText = data.city.name;
-    infoCard.appendChild(locationH1);
-    const city = createParagraphElement();
-    city.innerText = data.city.country;
-    infoCard.appendChild(city);
-    const cityImage = createDivElement();
-    cityImage.className = "cityImage";
-    cityImage.innerHTML = `<img src=${image.results[0].urls.regular} alt="City of" style="width:200px;height:200px">`;
-    infoCard.appendChild(cityImage);
+    const ingredientsHolder = createDivElement();
+    ingredientsHolder.className = ("ingredientHolder");
 }
 groceryList();
 console.log(main);
