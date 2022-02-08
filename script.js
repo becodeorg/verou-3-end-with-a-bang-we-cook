84f7f80d652810435884e9900e65ffb36e73c434
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-08 09:05:29 +0100

    "WIP Basic card structure"

---
+++ script.js
@@ -0,0 +1,28 @@
+//Ellen Seghers
+
+//TODO: Create cards
+    //Create elements you need for the cards
+    const div1 = document.createElement("div");
+    const image = document.createElement("img");
+    const div2 = document.createElement("div");
+    const pTag = document.createElement("p");
+    const div3 = document.createElement("div");
+    let button1 = document.createElement("button");
+    let button2 = document.createElement("button");
+
+    //Assign the type of button to the html
+    button1.innerHTML = "Add to week planning";
+    button2.innerHTML = "Go to website";
+
+    //Add the elements to the html
+    document.body.append(div1);
+    div1.appendChild(image);
+    div1.appendChild(div2);
+    div2.appendChild(pTag);
+    div1.appendChild(div3);
+    div3.appendChild(button1);
+    div3.appendChild(button2);
+
+
+    //TODO: Add classnames to the div tags
+    div1.className = "card";
