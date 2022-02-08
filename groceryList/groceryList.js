617ae4c80ed8f83ee05e858153745dbe6199d8df
unknown <cskeith01@gmail.com>
2022-02-08 10:49:56 +0100

    WIP on groceryList

--- groceryList/groceryList.js
+++ groceryList/groceryList.js
@@ -9,12 +9,16 @@ let day = () => {
         "Friday",
         "Saturday"
     );
- // TODO Get more date info for groceryList 
+    // TODO Get more date info for groceryList 
     for (i = 0; i < 7; i++) {
         console.log(week[(day.getDay() + 1 + i) % 7]);
     }
 }
-// day();
+
+
+// fetch('https://api.edamam.com/api/recipes/v2?type=public&q=british&app_id=dc33d4d0&app_key=4ed7538e5048146690cf86e13c0f1d1b')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 
 
 //  What should i do for the groceryList
@@ -23,6 +27,49 @@ let day = () => {
 // TODO Look in Variables for links
 // TODO Check out ingredients measure options
 // TODO create groceryList Holder
+const createDivElement = () => {
+    const newDiv = document.createElement("div");
+    return newDiv;
+}
+
+const createParagraphElement = () => {
+    const newParagraph = document.createElement("p");
+    return newParagraph;
+}
+
+const createH1Element = () => {
+    const newH1 = document.createElement("h1");
+    return newH1;
+}
+
+const groceryList = () => {
+    // Link to HTML
+    const main = document.querySelector("main");
+    // create
+    const groceryListBody = createDivElement();
+    groceryListBody.className = ("groceryListBody");
+    main.appendChild(groceryListBody);
+
+    const groceryListTitle = createH1Element();
+    groceryListTitle.innerHTML = ("Grocery List");
+    groceryListBody.appendChild(groceryListTitle);
+
+    const infoCard = createDivElement();
+
+    section.appendChild(infoCard);
+    const locationH1 = createH1Element();
+    locationH1.innerText = data.city.name;
+    infoCard.appendChild(locationH1);
+    const city = createParagraphElement();
+    city.innerText = data.city.country;
+    infoCard.appendChild(city);
+    const cityImage = createDivElement();
+    cityImage.className = "cityImage";
+    cityImage.innerHTML = `<img src=${image.results[0].urls.regular} alt="City of" style="width:200px;height:200px">`;
+    infoCard.appendChild(cityImage);
+}
+groceryList();
+console.log(main);
 // TODO create nodes for api data
 // TODO create check box ? 
 // TODO ingredients quantity's
