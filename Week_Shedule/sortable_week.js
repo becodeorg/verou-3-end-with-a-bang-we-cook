f6e909810338805bb65818a76233fdd23f386d71
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-02-09 09:30:36 +0100

    drag and drop days

--- Week_Shedule/sortable_week.js
+++ Week_Shedule/sortable_week.js
@@ -1,11 +1,20 @@
+//followed this guide: https://www.html5rocks.com/en/tutorials/dnd/basics//
+document.addEventListener('DOMContentLoaded', (event) => {
+var dragSrcEl=null;
 function handleDragStart(e) {
   this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
 function handleDragEnd(e) {
   this.style.opacity = '1';
+ 
+  return false;
 }
-
 function handleDragOver(e) {
   if (e.preventDefault) {
     e.preventDefault();
@@ -13,10 +22,32 @@ function handleDragOver(e) {
 
   return false;
 }
+function handleDrop(e) {
+  // this/e.target is current target element.
 
-let items = document.querySelectorAll('.week .day');
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+let items = document.querySelectorAll('#week .day');
+
 items.forEach(function (item) {
+  console.log(item);
   item.addEventListener('dragstart', handleDragStart);
+  item.addEventListener('dragover', handleDragOver);
   item.addEventListener('dragend', handleDragEnd);
+  item.addEventListener('drop', handleDrop);
+
+});
 
 });
