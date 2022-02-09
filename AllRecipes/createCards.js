c7bc74c455caa1ece0b3b1148faa99cf09696365
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-09 15:03:37 +0100

    "WIP change button images"

--- AllRecipes/createCards.js
+++ AllRecipes/createCards.js
@@ -1,2 +1,23 @@
 //Ellen Seghers
 
+//Get your a tag element out of your html by the classname
+const buttons = document.querySelectorAll(".plusImage");
+
+const changeButtonImage = (index) => {
+    const selectedElement = buttons[index]
+    if(selectedElement.src === "plusmark.png"){
+        selectedElement.classList.add("selected");
+        selectedElement.src = "checkmark.png";
+    }
+    else{
+        selectedElement.src = "plusmark.png";
+        selectedElement.classList.remove("selected");
+    }
+    console.log(selectedElement);
+}
+
+buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        changeButtonImage(index)
+    });
+})
