a39229e5b3d19496cb70ab6c75a7c34e48fc4ed2
Sa-raCha <sarabibimagdalena@gmail.com>
2022-02-11 17:02:31 +0100

    Final touches

--- script.js
+++ script.js
@@ -45,3 +45,24 @@ const makeCard = (onePerson, i) => {
     `
     container.innerHTML += content;
 }
+
+function hamburgerMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.hamburgerButton')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+    hamburgerMenu();
+
+    console.log("Push the Button");
+}
