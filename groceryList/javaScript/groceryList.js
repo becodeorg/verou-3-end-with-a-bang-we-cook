ee223d1ddb5ff8ee012b0ed91acffa3d798ffdb6
unknown <cskeith01@gmail.com>
2022-02-11 10:50:35 +0100

    WIP Fix date on List

--- groceryList/javaScript/groceryList.js
+++ groceryList/javaScript/groceryList.js
@@ -20,7 +20,6 @@ const mainElement = document.querySelector("main");
 
 let groceryListLoop = (data) => {
     for (let l = 0; l < data.recipe.label[l].length; l++) {
-
         // create groceryList Body
         const groceryListBody = createDivElement();
         groceryListBody.className = ("groceryListBody");
@@ -30,42 +29,38 @@ let groceryListLoop = (data) => {
         const groceryListTitle = createH3Element();
         groceryListTitle.innerHTML = (data.recipe.label);
         groceryListBody.appendChild(groceryListTitle);
-        ingredientsLoop(groceryListBody, dateLoop, data);
-    }
-    for (let i = 0; i < 7; i++) {
+
         // create date Holder
-       const date = createDivElement();
-       date.className = ("date");
-       groceryListBody.appendChild(date);
-       date.innerText = addEveryWeekDay();
-       console.log(date);
-       groceryListLoop(data);
-   }
-   for (let i = 0; i < data.recipe.ingredients.length; i++) {
-    
-    // create ingredientsHolder
-    const ingredientsHolder = createUlElement();
-    ingredientsHolder.className = ("ingredientsHolder");
-    groceryListBody.appendChild(ingredientsHolder);
+        const date = createDivElement();
+        date.className = ("date");
+        groceryListBody.appendChild(date);
+        date.innerHTML = addEveryWeekDay()[l];
+        console.log(date);
+    }
+    for (let i = 0; i < data.recipe.ingredients.length; i++) {
+        // create ingredientsHolder
+        const ingredientsHolder = createUlElement();
+        ingredientsHolder.className = ("ingredientsHolder");
+        groceryListBody.appendChild(ingredientsHolder);
 
-    // create checkBox
-    const checkBox = checkBoxElement();
-    checkBox.className = ("checkBox")
-    ingredientsHolder.appendChild(checkBox);
+        // create checkBox
+        const checkBox = checkBoxElement();
+        checkBox.className = ("checkBox")
+        ingredientsHolder.appendChild(checkBox);
 
-    let amount = createLiElement();
-    amount.innerHTML = data.recipe.ingredients[i].quantity.toPrecision(2);
-    ingredientsHolder.appendChild(amount);
+        let amount = createLiElement();
+        amount.innerHTML = data.recipe.ingredients[i].quantity.toPrecision(2);
+        ingredientsHolder.appendChild(amount);
 
-    let measure = createLiElement();
-    measure.innerHTML = data.recipe.ingredients[i].measure;
-    ingredientsHolder.appendChild(measure);
+        let measure = createLiElement();
+        measure.innerHTML = data.recipe.ingredients[i].measure;
+        ingredientsHolder.appendChild(measure);
 
-    let ingredientsParagraph = createLiElement();
-    ingredientsParagraph.className = "ingredientsLi";
-    ingredientsParagraph.innerHTML = data.recipe.ingredients[i].food;
-    ingredientsHolder.appendChild(ingredientsParagraph);
-}
+        let ingredientsParagraph = createLiElement();
+        ingredientsParagraph.className = "ingredientsLi";
+        ingredientsParagraph.innerHTML = data.recipe.ingredients[i].food;
+        ingredientsHolder.appendChild(ingredientsParagraph);
+    }
 
 }
 
